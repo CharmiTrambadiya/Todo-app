7b967068d35a35eaf31fabc180a650e8bfa14dd9
charmi <charmi@zuru.tech>
2019-03-18 19:35:36 +0530

    feat: add user-details app

--- src/components/todoitem.jsx
+++ src/components/todoitem.jsx
@@ -3,27 +3,35 @@ class TodoItems extends Component {
   constructor(props) {
     super(props);
     this.createTasks = this.createTasks.bind(this);
+    this.editItem = this.editItem.bind(this);
+    this.onChangeTextName = this.onChangeTextName.bind(this);
   }
-  delete(key) {
+  delete(e, key) {
+    e.stopPropagation();
     this.props.delete(key);
   }
   editItem(index) {
     // console.log(li);
-    var list = document.querySelector("ol");
-    var editList = document.querySelector("li");
-    editList.onclick = function () {
-      list.setAttribute("contentEditable", true);
-    };
+    // var list = document.querySelector("ol");
+    // var editList = document.querySelector("li");
+    //   list.setAttribute("contentEditable", true);
+    this.props.editItem(index);
+  }
+  onChangeTextName(e,index) {
+    var newItems = e.target.value;
+    console.log(newItems)
+    this.setState({ items: newItems });
+    this.props.onChangeTextName(index);
   }
   createTasks(item, index) {
     return (
-      <li className="names" key={index} onClick={e => this.editItem(e)}>
+      <li className="names" key={index} onClick={() => this.editItem(index)}>
         {item.isEdit ? (
-          <input type="text" className="inputName" value={item.text} />) : (
+          <input type="text" className="inputName" value={item.text}  onChange={e => this.onChangeTextName(e,index)} />) : (
             <span onClick={() => "inputName"}>{item.text}</span>
           )}
         {/* <input type='text' className="inputName" value={item.text} /> */}
-        <button onClick={() => this.delete(index)}>X</button>
+        <button onClick={(even) => this.delete(even, index)}>X</button>
       </li>
     );
   }
